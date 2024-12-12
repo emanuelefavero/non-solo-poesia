1c00619b9b7ea8a4aa3c4c0caa257103b7b6ad11
Emanuele Favero <emafaux89@gmail.com>
2024-12-12 15:45:24 +0100

    add admin and author roles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,32 @@
 'use client'
 
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useAuth,
+} from '@clerk/nextjs'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 export default function Component() {
   const pathname = usePathname()
   const isHomepage = pathname === '/'
+  const { userId } = useAuth()
+  const adminId = process.env.NEXT_PUBLIC_ADMIN_ID
+  const authorId = process.env.NEXT_PUBLIC_AUTHOR_ID
 
   return (
     <>
       {!isHomepage ? <Link href='/'>Vai alla Home</Link> : <div>Home</div>}
       <div className='flex items-center'>
-        <Link href='/create-post' className='mr-2'>
-          + Nuovo Post
-        </Link>
+        {(userId === adminId || userId === authorId) &&
+          pathname !== '/create-post' && (
+            <Link href='/create-post' className='mr-2'>
+              + Nuovo Post
+            </Link>
+          )}
         <SignedOut>
           <SignInButton />
         </SignedOut>
@@ -25,3 +37,6 @@ export default function Component() {
     </>
   )
 }
+
+// user.primaryEmailAddress.emailAddress
+// user.id
