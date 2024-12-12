c085705cb252344154520972ceb2687df0c39d03
Emanuele Favero <emafaux89@gmail.com>
2024-12-12 19:35:46 +0100

    add delete post popover

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { deletePost } from '@/app/actions'
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
 import { useRouter } from 'next/navigation'
 
 interface Props {
@@ -11,14 +12,35 @@ export default function Component({ slug }: Props) {
   const router = useRouter()
 
   return (
-    <button
-      onClick={async () => {
-        await deletePost(slug)
-        router.push('/') // Redirect home
-      }}
-      className='text-rose-600 dark:text-rose-500'
-    >
-      Elimina
-    </button>
+    <Popover>
+      <PopoverButton className='block rounded-sm font-semibold text-rose-500/80 focus:outline-none data-[active]:text-rose-500 data-[hover]:text-rose-500 data-[focus]:outline-1 data-[focus]:outline-gray-300 dark:data-[focus]:outline-white'>
+        Elimina
+      </PopoverButton>
+      <PopoverPanel
+        transition
+        anchor='bottom'
+        className='ml-4 divide-y divide-gray-50 rounded-xl border border-gray-200 bg-gray-50 px-4 py-2 transition duration-200 ease-in-out [--anchor-gap:var(--spacing-5)] data-[closed]:-translate-y-1 data-[closed]:opacity-0 dark:divide-neutral-900 dark:border-gray-700 dark:bg-neutral-900'
+      >
+        <p className='select-none text-gray-600 dark:text-gray-400'>
+          Eliminare questo post?
+        </p>
+
+        <div className='flex gap-2'>
+          <button
+            onClick={async () => {
+              await deletePost(slug)
+              router.push('/') // Redirect home
+            }}
+            className='block rounded-lg px-3 py-2 text-rose-500 transition hover:bg-black/5 dark:hover:bg-white/5'
+          >
+            Elimina
+          </button>
+
+          <PopoverButton className='block rounded-lg px-3 py-2 text-gray-600 transition hover:bg-black/5 dark:text-gray-400 dark:hover:bg-white/5'>
+            Annulla
+          </PopoverButton>
+        </div>
+      </PopoverPanel>
+    </Popover>
   )
 }
