5a5884ce693559500105ae992c9e9cbf45f3d559
Emanuele Favero <emafaux89@gmail.com>
2024-12-24 00:42:34 +0100

    add url state in search route

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,16 +1,32 @@
 'use client'
 
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+
 export default function Page() {
-  const handleClick = async () => {
-    alert('Hello')
-  }
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const color = searchParams.get('color') || 'red'
 
   return (
     <>
       <h1>Test</h1>
-      <button onClick={handleClick} className='w-fit rounded bg-green-500 px-4'>
-        Click
-      </button>
+
+      <select
+        id='color'
+        value={color}
+        onChange={(e) => {
+          router.replace(`${pathname}?color=${e.target.value}`)
+        }}
+      >
+        <option value='red'>Red</option>
+        <option value='blue'>Blue</option>
+      </select>
+
+      <p className={color === 'red' ? 'text-red-500' : 'text-blue-500'}>
+        Hello
+      </p>
     </>
   )
 }
