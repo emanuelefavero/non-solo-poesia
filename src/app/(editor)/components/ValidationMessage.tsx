ad4cb07e1961e7214e5c4a065eab8ce3a927a44c
Emanuele Favero <emafaux89@gmail.com>
2025-01-03 00:30:24 +0100

    add opacity transition to message in Editor

--- src/app/(editor)/components/ValidationMessage.tsx
+++ src/app/(editor)/components/ValidationMessage.tsx
@@ -11,7 +11,9 @@ export default function Component() {
 
   return (
     <p
-      className={`mt-4 font-medium ${message.type === 'error' ? 'text-rose-600 dark:text-rose-500' : 'text-green-600 dark:text-green-500'}`}
+      className={`mt-4 min-h-[36px] font-medium transition-opacity duration-300 ${message.type === 'error' ? 'text-rose-600 dark:text-rose-500' : 'text-green-600 dark:text-green-500'} ${
+        message.text ? 'opacity-100' : 'opacity-0'
+      }`}
     >
       {message.text}
     </p>
