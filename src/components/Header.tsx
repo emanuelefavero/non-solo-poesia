4b8ed3a4af667ee25accb443a8da667e1cd85ac4
Emanuele Favero <emafaux89@gmail.com>
2025-01-02 21:31:18 +0100

    add search link in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,6 +13,7 @@ import { usePathname } from 'next/navigation'
 export default function Component() {
   const pathname = usePathname()
   const isHomepage = pathname === '/'
+  const isSearchPage = pathname === '/search'
   const { userId } = useAuth()
   const adminId = process.env.NEXT_PUBLIC_ADMIN_ID
   const authorId = process.env.NEXT_PUBLIC_AUTHOR_ID
@@ -21,12 +22,13 @@ export default function Component() {
     <>
       {!isHomepage ? <Link href='/'>Vai alla Home</Link> : <div>Home</div>}
       <div className='flex items-center'>
-        {(userId === adminId || userId === authorId) &&
-          pathname !== '/create-post' && (
-            <Link href='/create-post' className='mr-2'>
-              + Nuovo Post
-            </Link>
-          )}
+        <nav className='mr-3 flex gap-3'>
+          {!isSearchPage ? <Link href='/search'>Cerca</Link> : <div>Cerca</div>}
+          {(userId === adminId || userId === authorId) &&
+            pathname !== '/create-post' && (
+              <Link href='/create-post'>+ Nuovo Post</Link>
+            )}
+        </nav>
         <SignedOut>
           <SignInButton>
             <button>Accedi</button>
