719052fe6bea8f0fe50ce5efcec10fa3e8c4ad2b
Emanuele Favero <emafaux89@gmail.com>
2025-01-03 00:52:30 +0100

    add margin and padding in search page and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
             <Header />
           </header>
 
-          <main className='flex flex-col gap-4 p-4'>{children}</main>
+          <main className='flex flex-col gap-4 px-4 py-16'>{children}</main>
 
           <footer className='flex w-full flex-col flex-wrap items-center justify-center gap-2 bg-gray-500/10 p-4 text-sm'>
             <Footer />
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -57,7 +57,7 @@ export default function Page() {
       </div>
 
       <p
-        className={`mt-1 min-h-[36px] font-semibold italic text-yellow-700 transition-opacity duration-300 dark:text-yellow-500 ${
+        className={`mb-4 mt-1 min-h-[36px] font-semibold italic text-yellow-700 transition-opacity duration-300 dark:text-yellow-500 ${
           message ? 'opacity-100' : 'opacity-0'
         }`}
       >
