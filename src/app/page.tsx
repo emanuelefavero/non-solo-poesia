0136e451f65aa4b4bc3bac15c5c6dd1138ea149e
Emanuele Favero <emafaux89@gmail.com>
2025-01-02 12:47:45 +0100

    add PaginationLink component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,29 +86,28 @@ function Pagination({ currentPage, totalPages }: PaginationProps) {
   return (
     <div className='mt-4 flex h-[34px] items-center justify-center'>
       {currentPage > 1 && (
-        <Link
-          href='/?page=1'
-          className='mx-0.5 rounded px-4 py-1 font-bold text-black transition-transform duration-200 hover:bg-blue-500 hover:text-white hover:no-underline active:scale-95 dark:text-white'
-          aria-label='Prima pagina'
+        <PaginationLink
+          href={`/?page=1`}
+          ariaLabel='Prima pagina'
           title='Prima pagina'
         >
           <div className='flex h-[24px] items-center'>
             <ChevronLeftIcon className='h-[16px]' />
           </div>
-        </Link>
+        </PaginationLink>
       )}
 
       {pages.map((page, index) =>
         typeof page === 'number' ? (
-          <Link
+          <PaginationLink
             key={index}
             href={`/?page=${page}`}
-            className={`mx-0.5 rounded px-4 py-1 text-black transition-transform duration-200 hover:bg-blue-500 hover:text-white hover:no-underline active:scale-95 dark:text-white ${
-              page === currentPage ? 'bg-blue-500/80 text-white' : ''
-            }`}
+            className={page === currentPage ? 'bg-blue-500/80 text-white' : ''}
+            ariaLabel={`Pagina ${page}`}
+            title={`Pagina ${page}`}
           >
             {page}
-          </Link>
+          </PaginationLink>
         ) : (
           <span
             key={index}
@@ -120,18 +119,47 @@ function Pagination({ currentPage, totalPages }: PaginationProps) {
       )}
 
       {currentPage < totalPages && (
-        <Link
+        <PaginationLink
           href={`/?page=${totalPages}`}
-          className='mx-0.5 rounded px-4 py-1 font-bold text-black transition-transform duration-200 hover:bg-blue-500 hover:text-white hover:no-underline active:scale-95 dark:text-white'
-          aria-label='Ultima pagina'
+          ariaLabel='Ultima pagina'
           title='Ultima pagina'
         >
           <div className='flex h-[24px] items-center'>
             <ChevronRightIcon className='h-[16px]' />
           </div>
-        </Link>
+        </PaginationLink>
       )}
     </div>
+  )
+}
+
+interface PaginationLinkProps {
+  href: string
+  children: React.ReactNode
+  className?: string
+  ariaLabel?: string
+  title?: string
+  page?: number
+}
+
+function PaginationLink({
+  href,
+  children,
+  className,
+  ariaLabel,
+  title,
+}: PaginationLinkProps) {
+  return (
+    <Link
+      href={href}
+      className={`mx-0.5 rounded px-4 py-1 text-black transition-transform duration-200 hover:bg-blue-500 hover:text-white hover:no-underline active:scale-95 dark:text-white ${
+        className || ''
+      }`}
+      aria-label={ariaLabel}
+      title={title}
+    >
+      {children}
+    </Link>
   )
 }
 
