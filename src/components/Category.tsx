67ba783214864667cde5aee6f6d398cf72f1a305
Emanuele Favero <emafaux89@gmail.com>
2025-01-07 22:24:09 +0100

    add className prop to Category component

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -2,11 +2,14 @@ import type { CategoryNames } from '@/types'
 
 interface Props {
   category: CategoryNames
+  className?: string
 }
 
-export default function Component({ category }: Props) {
+export default function Component({ category, className = '' }: Props) {
   return (
-    <span className='text-sm font-semibold uppercase text-pink-500 dark:text-pink-400'>
+    <span
+      className={`text-sm font-semibold uppercase text-pink-500 dark:text-pink-400 ${className}`}
+    >
       {category}
     </span>
   )
