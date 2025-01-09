c7bbceccb309138cdfd4bf7f08fd7af182db5d82
Emanuele Favero <emafaux89@gmail.com>
2025-01-09 05:10:44 +0100

    translate category url to italian - categoria

--- src/components/CategoryLink.tsx
+++ src/components/CategoryLink.tsx
@@ -11,7 +11,7 @@ type Props = {
 export default function Component({ name, slug, className }: Props) {
   return (
     <Link
-      href={`/category/${slug ? slug : convertNameToSlug(name)}`}
+      href={`/categoria/${slug ? slug : convertNameToSlug(name)}`}
       title={`Vai a ${name}`}
       aria-label={`Vai a ${name}`}
       className={`font-medium uppercase text-pink-600 transition-all duration-300 hover:text-pink-800 hover:no-underline active:scale-95 dark:text-pink-400 dark:hover:text-pink-300 ${className || ''}`}
