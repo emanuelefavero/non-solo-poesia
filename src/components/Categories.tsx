8683564fbe46bee69310fdc8260ecbe7780757f9
Emanuele Favero <emafaux89@gmail.com>
2025-01-09 03:53:11 +0100

    add convertNameToSlug to CategoryLink

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,16 +1,32 @@
 import { categories } from '@/data/categories'
+import type { CategoryNames } from '@/types'
 import Link from 'next/link'
 
 export default function Component() {
   return categories.map((category) => (
-    <Link
-      key={category.id}
-      href={`/category/${category.slug}`}
-      title={`Vai a ${category.name}`}
-      aria-label={`Vai a ${category.name}`}
-      className='font-semibold uppercase text-pink-500 dark:text-pink-400'
-    >
-      {category.name}
-    </Link>
+    <CategoryLink key={category.id} name={category.name} slug={category.slug} />
   ))
+}
+
+type Props = {
+  name: CategoryNames
+  slug?: string
+  className?: string
+}
+
+function CategoryLink({ name, slug, className }: Props) {
+  return (
+    <Link
+      href={`/category/${slug ? slug : convertNameToSlug(name)}`}
+      title={`Vai a ${name}`}
+      aria-label={`Vai a ${name}`}
+      className={`font-semibold uppercase text-pink-500 dark:text-pink-400 ${className || ''}`}
+    >
+      {name}
+    </Link>
+  )
+}
+
+function convertNameToSlug(name: string) {
+  return name.toLowerCase().replace(/\s/g, '-')
 }
