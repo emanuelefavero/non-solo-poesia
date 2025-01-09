f2ad0b5ea8033c5b2b12aeeed702a29725acf00e
Emanuele Favero <emafaux89@gmail.com>
2025-01-09 05:40:31 +0100

    add headless ui drodown menu open state

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -5,7 +5,7 @@ export default function Component() {
     <Menu as='div' className='relative inline-block text-left'>
       <div>
         <MenuButton className='inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'>
-          Menu
+          {({ open }) => <>{open ? 'Close' : 'Open'}</>}
         </MenuButton>
       </div>
 
