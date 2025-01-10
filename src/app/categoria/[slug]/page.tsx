c02127f8baea1d890d9ff682a82df3b59022f032
Emanuele Favero <emafaux89@gmail.com>
2025-01-10 06:32:53 +0100

    refactor /categoria/[slug] page

--- src/app/categoria/[slug]/page.tsx
+++ src/app/categoria/[slug]/page.tsx
@@ -2,14 +2,18 @@ import { CATEGORY_SLUGS } from '@/config/categories'
 import type { CategorySlug } from '@/types'
 import { redirect } from 'next/navigation'
 
-type Props = { params: Promise<{ slug: string }> }
+type Props = { params: Promise<{ slug: string; order_by?: string }> }
 
 export default async function Page({ params }: Props) {
-  const slug = (await params).slug
+  const { slug } = await params
 
   if (!CATEGORY_SLUGS.includes(slug as CategorySlug)) {
     redirect('/')
   }
 
-  return <h1>{slug}</h1>
+  return (
+    <>
+      <h1>{slug}</h1>
+    </>
+  )
 }
