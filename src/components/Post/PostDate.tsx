f0dffb38742b3a89f7bc77a4f5df1418c960d6b9
Emanuele Favero <emafaux89@gmail.com>
2025-01-13 22:56:55 +0100

    add - do not show updated date if is updated on same day

--- src/components/Post/PostDate.tsx
+++ src/components/Post/PostDate.tsx
@@ -1,14 +1,35 @@
 type Props = {
-  published_at: string
+  published_at: string // e.g. 2024-12-10 07:23:57.257+00
   updated_at?: string | null
 }
 
 export default function Component({ published_at, updated_at }: Props) {
-  const date = updated_at || published_at // ? 2024-12-10 07:23:57.257+00
+  // Check if the updated date is at least one day later than the published date
+  const isAtLeastOneDayLater = (published: string, updated: string) => {
+    const publishedDate = new Date(published)
+    const updatedDate = new Date(updated)
+
+    return (
+      updatedDate.getFullYear() > publishedDate.getFullYear() ||
+      (updatedDate.getFullYear() === publishedDate.getFullYear() &&
+        updatedDate.getMonth() > publishedDate.getMonth()) ||
+      (updatedDate.getFullYear() === publishedDate.getFullYear() &&
+        updatedDate.getMonth() === publishedDate.getMonth() &&
+        updatedDate.getDate() > publishedDate.getDate())
+    )
+  }
+
+  const date =
+    updated_at && isAtLeastOneDayLater(published_at, updated_at)
+      ? updated_at
+      : published_at
 
   return (
     <p className='mt-1 text-sm font-medium italic text-zinc-500 dark:text-zinc-400'>
-      {updated_at ? 'Aggiornato' : 'Pubblicato'} il{' '}
+      {updated_at && isAtLeastOneDayLater(published_at, updated_at)
+        ? 'Aggiornato'
+        : 'Pubblicato'}{' '}
+      il{' '}
       {new Date(date)
         .toLocaleDateString('it-IT', {
           day: 'numeric',
