08f57222d600354ecc430bb014d820b269becb92
Emanuele Favero <emafaux89@gmail.com>
2025-01-13 22:07:12 +0100

    add PostDate component

---
+++ src/components/Post/PostDate.tsx
@@ -0,0 +1,17 @@
+type Props = { date: Date }
+
+export default function Component({ date }: Props) {
+  return (
+    <p className='mt-1 text-sm font-medium italic text-zinc-500 dark:text-zinc-400'>
+      Pubblicato il{' '}
+      {new Date(date)
+        .toLocaleDateString('it-IT', {
+          day: 'numeric',
+          month: 'long',
+          year: 'numeric',
+        })
+        // Capitalize the first letter of the month
+        .replace(/(\b\w)/g, (char) => char.toUpperCase())}
+    </p>
+  )
+}
