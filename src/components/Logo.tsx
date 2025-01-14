79255d155b38b6202cf98cd413e5c31b35fb2c54
Emanuele Favero <emafaux89@gmail.com>
2025-01-14 02:57:00 +0100

    add - handle refresh page with useRouter

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,16 @@
 import { dancing_script } from '@/app/fonts'
 import { TITLE } from '@/config/index'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 type Props = {
   isHomepage: boolean
 }
 
 export default function Component({ isHomepage }: Props) {
+  const router = useRouter()
   const handleRefreshPage = () => {
-    window.scrollTo(0, 0)
-    window.location.reload()
+    router.push('/')
   }
 
   const commonStyles = `${dancing_script.className} text-base 5xs:text-xl 4xs:text-2xl 3xs:text-4xl 2xs:text-5xl xs:text-6xl sm:text-7xl text-center font-bold text-zinc-800 dark:text-zinc-100`
@@ -21,8 +22,6 @@ export default function Component({ isHomepage }: Props) {
       </button>
     )
   }
-
-  // TODO Add page and order_by search parameter to the link (page=1&order_by=published_at). Check if the Order By 'Data' or 'Titolo' gets updated properly when the page is reloaded, if not, you have to convert this component to a client component and use the order_by state or try to only update the page state to 1 and see if the behavior is fine.
 
   return (
     <Link href='/' className='hover:no-underline'>
