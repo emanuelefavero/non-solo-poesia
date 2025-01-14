94c427f0f10f1e67e199c15e257884b9f47f44b3
Emanuele Favero <emafaux89@gmail.com>
2025-01-14 08:08:36 +0100

    add - same path state to CategoryLink

--- src/components/CategoryLink.tsx
+++ src/components/CategoryLink.tsx
@@ -1,6 +1,7 @@
 import type { CategoryName } from '@/types'
 import { convertNameToSlug } from '@/utils/slug'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 type Props = {
   name: CategoryName
@@ -9,12 +10,27 @@ type Props = {
 }
 
 export default function Component({ name, slug, className }: Props) {
+  const pathname = usePathname()
+  const href = `/categoria/${slug ? slug : convertNameToSlug(name)}`
+  const isSamePath = pathname === href
+  const commonStyles = 'font-medium uppercase transition-all duration-300'
+
+  if (isSamePath) {
+    return (
+      <span
+        className={`${commonStyles} text-black dark:text-white ${className || ''}`}
+      >
+        {name}
+      </span>
+    )
+  }
+
   return (
     <Link
-      href={`/categoria/${slug ? slug : convertNameToSlug(name)}`}
+      href={href}
       title={`Vai a ${name}`}
       aria-label={`Vai a ${name}`}
-      className={`font-medium uppercase text-pink-600 transition-all duration-300 hover:text-pink-800 hover:no-underline active:scale-95 dark:text-pink-400 dark:hover:text-pink-300 ${className || ''}`}
+      className={`${commonStyles} text-pink-600 hover:text-pink-800 hover:no-underline active:scale-95 dark:text-pink-400 dark:hover:text-pink-300 ${className || ''}`}
     >
       {name}
     </Link>
