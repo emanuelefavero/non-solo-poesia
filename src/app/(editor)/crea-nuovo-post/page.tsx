a4c1a2422e85d83c75746ec158f36d0af0d2d03e
Emanuele Favero <emafaux89@gmail.com>
2025-01-14 10:52:14 +0100

    add title, description metadata to static pages

--- src/app/(editor)/crea-nuovo-post/page.tsx
+++ src/app/(editor)/crea-nuovo-post/page.tsx
@@ -1,6 +1,13 @@
 import Editor from '@/app/(editor)/components/Editor'
+import { TITLE } from '@/data/title'
 import { auth } from '@clerk/nextjs/server'
+import type { Metadata } from 'next'
 import { redirect } from 'next/navigation'
+
+export const metadata: Metadata = {
+  title: `Nuovo post - ${TITLE}`,
+  description: `Crea un nuovo post per il blog ${TITLE}. Questa pagina è riservata agli amministratori e agli autori.`,
+}
 
 export default async function Page() {
   const { userId } = await auth()
@@ -14,7 +21,7 @@ export default async function Page() {
 
   return (
     <>
-      <h1 className='mb-4'>Aggiungi nuovo post</h1>
+      <h1 className='mb-4'>Crea nuovo post</h1>
       <Editor />
     </>
   )
--- src/app/cerca/page.tsx
+++ src/app/cerca/page.tsx
@@ -1,6 +1,13 @@
 import FoundPosts from '@/app/cerca/components/FoundPosts'
 import SearchForm from '@/app/cerca/components/SearchForm'
 import SearchMessage from '@/app/cerca/components/SearchMessage'
+import { TITLE } from '@/data/title'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: `Cerca - ${TITLE}`,
+  description: `Cerca un post nel blog ${TITLE} digitando una parola chiave del titolo`,
+}
 
 export default function Page() {
   return (
