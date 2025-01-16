d14fcb850c8c7592ba559cce4aed4bdabf77d2fd
Emanuele Favero <emafaux89@gmail.com>
2025-01-16 04:34:39 +0100

    add border on category title

--- src/app/categoria/[slug]/page.tsx
+++ src/app/categoria/[slug]/page.tsx
@@ -52,7 +52,10 @@ export default async function Page({ params, searchParams }: Props) {
   return (
     <>
       <OrderBy currentOrderBy={currentOrderBy} />
-      <h1>{category}</h1>
+      <h1 className="relative w-fit after:mt-2 after:block after:h-[3px] after:w-14 after:rounded-full after:bg-pink-600 after:content-[''] dark:after:bg-pink-400">
+        {category}
+      </h1>
+
       <PostList posts={posts} />
       <Pagination
         currentPage={currentPage}
