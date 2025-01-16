45fb6698cbda986cee9e17e99c372c6805bcc6c4
Emanuele Favero <emafaux89@gmail.com>
2025-01-16 04:57:21 +0100

    add Title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  children: React.ReactNode
+  className?: string
+}
+
+export default function Component({ children, className }: Props) {
+  return (
+    <h1
+      className={`relative mb-2 w-fit text-2xl font-normal uppercase tracking-wider after:mt-2 after:block after:h-[3px] after:w-14 after:rounded-full after:bg-pink-600 after:content-[''] dark:after:bg-pink-400 ${className || ''}`}
+    >
+      {children}
+    </h1>
+  )
+}
