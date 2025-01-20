56995d58939b8ffe6cf814e3dd5c324da449f61c
Emanuele Favero <emafaux89@gmail.com>
2025-01-20 23:06:18 +0100

    remove border from aside

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -5,9 +5,7 @@ type Props = {
 
 export default function Component({ children, className }: Props) {
   return (
-    <aside
-      className={`relative hidden rounded-md border border-pink-400 p-3 lg:block ${className}`}
-    >
+    <aside className={`relative hidden rounded-md p-3 lg:block ${className}`}>
       {children}
     </aside>
   )
