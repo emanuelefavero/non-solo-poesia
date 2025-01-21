bdc4da53e7d042d107f3832ef13c0b855e2ce931
Emanuele Favero <emafaux89@gmail.com>
2025-01-21 09:49:20 +0100

    add className prop to PostAuthor component

--- src/components/Post/PostAuthor.tsx
+++ src/components/Post/PostAuthor.tsx
@@ -1,12 +1,20 @@
 type Props = {
   author: string
+  className?: string
+  authorClassName?: string
 }
 
-export default function Component({ author }: Props) {
+export default function Component({
+  author,
+  className,
+  authorClassName,
+}: Props) {
   return (
-    <span>
+    <span className={`${className}`}>
       Scritto da{' '}
-      <span className='text-pink-600 dark:text-pink-400'>{author}</span>
+      <span className={`text-pink-600 dark:text-pink-400 ${authorClassName}`}>
+        {author}
+      </span>
     </span>
   )
 }
