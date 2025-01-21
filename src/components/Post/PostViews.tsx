9d8dbcc74bd982838514abffa8212caebc72634a
Emanuele Favero <emafaux89@gmail.com>
2025-01-21 09:51:15 +0100

    add className prop to PostViews component

--- src/components/Post/PostViews.tsx
+++ src/components/Post/PostViews.tsx
@@ -3,15 +3,17 @@ import { formatViews } from '@/utils/views'
 
 type Props = {
   views: number
+  className?: string
+  iconClassName?: string
 }
 
-export default function Component({ views }: Props) {
+export default function Component({ views, className, iconClassName }: Props) {
   return (
     <span
-      className='flex select-none items-center gap-1 text-sm text-zinc-500 dark:text-zinc-400'
+      className={`flex select-none items-center gap-1 text-sm text-zinc-500 dark:text-zinc-400 ${className}`}
       title={`${formatViews(views)} Visualizzazioni`}
     >
-      <BsEyeIcon className='relative inline-block h-5 w-5' />
+      <BsEyeIcon className={`relative inline-block h-5 w-5 ${iconClassName}`} />
       {formatViews(views)}
     </span>
   )
