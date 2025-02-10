53bb43d5ba3998350c0944fa11e13f901898d5d1
Emanuele Favero <emafaux89@gmail.com>
2025-02-10 11:02:44 +0100

    set ad banner width in md screens to 768px

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -1,7 +1,7 @@
 export default function Component() {
   return (
     <div className='hidden place-items-center text-center text-zinc-600/35 4xs:grid dark:text-zinc-400/35'>
-      <div className='grid h-[50px] w-full select-none place-items-center rounded-md border-2 border-dotted border-zinc-800/20 bg-[var(--header-background)] text-sm font-semibold uppercase xs:h-[90px] md:w-[calc(375px+375px+1rem)] lg:w-full dark:border-zinc-200/20'>
+      <div className='grid h-[50px] w-full select-none place-items-center rounded-md border-2 border-dotted border-zinc-800/20 bg-[var(--header-background)] text-sm font-semibold uppercase xs:h-[90px] md:w-[768px] lg:w-full dark:border-zinc-200/20'>
         ADV
       </div>
     </div>
