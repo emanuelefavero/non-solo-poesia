3db7b030df8a3db6fe9fe6bc8e3263e0222e802b
Emanuele Favero <emafaux89@gmail.com>
2025-02-12 06:39:05 +0100

    fix NewsletterForm responsivness

--- src/components/NewsletterForm.tsx
+++ src/components/NewsletterForm.tsx
@@ -1,9 +1,11 @@
 export default function Component() {
   return (
-    <div className='flex select-none flex-col items-center justify-center gap-2 md:flex-row md:gap-3'>
-      <h2 className='text-lg uppercase'>Iscriviti alla Newsletter</h2>
+    <div className='hidden select-none flex-col flex-wrap items-center justify-center gap-2 5xs:flex md:flex-row md:gap-3'>
+      <h2 className='text-center text-lg uppercase'>
+        Iscriviti alla Newsletter
+      </h2>
 
-      <form className='flex items-center justify-center gap-2'>
+      <form className='flex flex-wrap items-center justify-center gap-2'>
         <label htmlFor='email' className='sr-only text-base'>
           Email{' '}
           <sup className='select-none align-top text-lg font-bold text-pink-600 dark:text-pink-400'>
