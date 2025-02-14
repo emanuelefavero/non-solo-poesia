36c73ab53266410a98d42241e3bc72436443abf3
Emanuele Favero <emafaux89@gmail.com>
2025-02-14 08:53:55 +0100

    move savePost function to separate file

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -135,3 +135,35 @@ export async function incrementPostViews(slug: string) {
     WHERE slug = ${slug}
   `
 }
+
+export async function savePost(post: Post) {
+  const sql = neon(process.env.DATABASE_URL as string)
+
+  await sql`
+  INSERT INTO posts (
+    id,
+    slug,
+    title,
+    description,
+    cover_image,
+    cover_image_cloudinary,
+    content,
+    author,
+    category,
+    published_at,
+    updated_at
+  ) VALUES (
+    ${post.id},
+    ${post.slug},
+    ${post.title},
+    ${post.description},
+    ${post.cover_image},
+    ${post.cover_image_cloudinary},
+    ${post.content},
+    ${post.author},
+    ${post.category},
+    ${post.published_at},
+    ${post.updated_at}
+  )
+  `
+}
