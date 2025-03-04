4924f1fcf61792cac7fceb19f4d05e7e43861bee
Emanuele Favero <emafaux89@gmail.com>
2025-03-04 16:56:12 +0100

    setup eslint for jest

--- src/app/test/page.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Page from './page'
-
-describe('Page', () => {
-  test('it renders', () => {
-    render(<Page />)
-    expect(screen.getByText(/hello world/i)).toBeInTheDocument()
-  })
-})
