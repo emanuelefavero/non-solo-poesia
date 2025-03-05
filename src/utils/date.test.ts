4c88b5521d1410f7d28bc017a397bea204f1abf6
Emanuele Favero <emafaux89@gmail.com>
2025-03-05 16:53:55 +0100

    add tests for utils/date isMonthDay1or8or11

--- src/utils/date.test.ts
+++ src/utils/date.test.ts
@@ -1,4 +1,4 @@
-import { isAtLeastOneDayLater } from './date'
+import { isAtLeastOneDayLater, isMonthDay1or8or11 } from './date'
 
 describe('isAtLeastOneDayLater', () => {
   it('should return false if updated_at is undefined', () => {
@@ -19,5 +19,23 @@ describe('isAtLeastOneDayLater', () => {
 
   it('should return true if updated_at is later than published_at by one year', () => {
     expect(isAtLeastOneDayLater('2025-01-01', '2026-01-01')).toBe(true)
+  })
+})
+
+describe('isMonthDay1or8or11', () => {
+  it('should return true if the day of the month is 1', () => {
+    expect(isMonthDay1or8or11(new Date('2025-01-01'))).toBe(true)
+  })
+
+  it('should return true if the day of the month is 8', () => {
+    expect(isMonthDay1or8or11(new Date('2025-01-08'))).toBe(true)
+  })
+
+  it('should return true if the day of the month is 11', () => {
+    expect(isMonthDay1or8or11(new Date('2025-01-11'))).toBe(true)
+  })
+
+  it('should return false if the day of the month is not 1, 8 or 11', () => {
+    expect(isMonthDay1or8or11(new Date('2025-01-02'))).toBe(false)
   })
 })
