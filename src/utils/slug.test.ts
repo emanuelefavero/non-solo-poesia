823758257f84096827c803beb557860ec75668e1
Emanuele Favero <emafaux89@gmail.com>
2025-03-05 16:32:06 +0100

    add test for utils/slug convertSlugToName

--- src/utils/slug.test.ts
+++ src/utils/slug.test.ts
@@ -1,7 +1,13 @@
-import { convertNameToSlug } from './slug'
+import { convertNameToSlug, convertSlugToName } from './slug'
 
 describe('convertNameToSlug', () => {
   it('should convert name to slug', () => {
     expect(convertNameToSlug('Ab Dc')).toBe('ab-dc')
+  })
+})
+
+describe('convertSlugToName', () => {
+  it('should convert slug to name', () => {
+    expect(convertSlugToName('ab-cd')).toBe('Ab Cd')
   })
 })
