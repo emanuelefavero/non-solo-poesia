6602b66e3d41fbaf141aff9735b2300a3b3eaba4
Emanuele Favero <emafaux89@gmail.com>
2025-03-07 23:31:26 +0100

    add contactEmail global variable to ContactUs

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,18 +1,17 @@
+import { contactEmail } from '@/data/email'
 import Link from 'next/link'
 
 export default function Component() {
-  const email = 'info@nonsolopoesia.it'
-
   return (
     <div className='hidden flex-col flex-wrap items-center justify-center gap-2 5xs:flex md:flex-row md:gap-3'>
       <h2 className='text-center text-lg uppercase'>Contattaci</h2>
 
       <div className='flex flex-wrap items-center justify-center gap-2'>
         <Link
-          href={`mailto:${email}`}
+          href={`mailto:${contactEmail}`}
           className='text-base text-pink-700 hover:underline dark:text-pink-400'
         >
-          {email}
+          {contactEmail}
         </Link>
       </div>
     </div>
