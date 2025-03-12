19a587af2f7580027bfd7353dcc8ac20135112e1
Emanuele Favero <emafaux89@gmail.com>
2025-03-12 10:03:53 +0100

    add e2e test - Post detail page has correct title

--- tests/seo/title.spec.ts
+++ tests/seo/title.spec.ts
@@ -10,3 +10,11 @@ test('Search page has correct title', async ({ page }) => {
   await page.goto('/cerca')
   await expect(page).toHaveTitle(`Cerca - ${TITLE}`)
 })
+
+test('Post detail page has correct title', async ({ page }) => {
+  const postSlug = 'sogni-nella-notte'
+  const postTitle = 'Sogni nella Notte'
+
+  await page.goto(`/post/${postSlug}`)
+  await expect(page).toHaveTitle(`${postTitle} - ${TITLE}`)
+})
