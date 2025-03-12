c5f551ef695a8790efe1083e444b338e2839a55e
Emanuele Favero <emafaux89@gmail.com>
2025-03-12 04:08:02 +0100

    add e2e test - Search page has correct title

--- tests/seo/title.spec.ts
+++ tests/seo/title.spec.ts
@@ -3,6 +3,10 @@ import { expect, test } from '@playwright/test'
 
 test('Homepage has correct title', async ({ page }) => {
   await page.goto('/')
-
   await expect(page).toHaveTitle(TITLE)
+})
+
+test('Search page has correct title', async ({ page }) => {
+  await page.goto('/cerca')
+  await expect(page).toHaveTitle(`Cerca - ${TITLE}`)
 })
