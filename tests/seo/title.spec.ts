4f4adca68df5f77a908cb938e7a859b38873e9cb
Emanuele Favero <emafaux89@gmail.com>
2025-03-12 03:53:06 +0100

    move e2e test to tests/seo/title.spec.ts

--- tests/seo/title.spec.ts
+++ tests/seo/title.spec.ts
@@ -1,7 +1,7 @@
 import { TITLE } from '@/data/title'
 import { expect, test } from '@playwright/test'
 
-test('has title', async ({ page }) => {
+test('Homepage has correct title', async ({ page }) => {
   await page.goto('/')
 
   await expect(page).toHaveTitle(TITLE)
