e935d79e5a0b5d007d743fbbd3cdc616ee34ca11
Emanuele Favero <emafaux89@gmail.com>
2025-03-12 17:39:19 +0100

    Reorder route e2e tests as they appear in the /app directory

--- e2e/seo/title.spec.ts
+++ e2e/seo/title.spec.ts
@@ -4,16 +4,28 @@ import { POST_NOT_FOUND_MESSAGE } from '@/data/post'
 import { TITLE } from '@/data/title'
 import { expect, test } from '@playwright/test'
 
-// TODO Reorder routes as they appear inside the /app directory
-
 test('Homepage has correct title', async ({ page }) => {
   await page.goto('/')
   await expect(page).toHaveTitle(TITLE)
 })
 
+test('Category page has correct title', async ({ page }) => {
+  const category = categories[0]
+  const { name } = category
+
+  await page.goto(`/categoria/${category.slug}`)
+  await expect(page).toHaveTitle(`${name} - ${TITLE}`)
+})
+
 test('Search page has correct title', async ({ page }) => {
   await page.goto('/cerca')
   await expect(page).toHaveTitle(`Cerca - ${TITLE}`)
+})
+
+test('Newsletter success page has correct title', async ({ page }) => {
+  // ? pass email query param to prevent redirect
+  await page.goto(`/newsletter-success?email=${TEST_EMAIL}`)
+  await expect(page).toHaveTitle(`Iscrizione completata - ${TITLE}`)
 })
 
 test('Post detail page has correct title', async ({ page }) => {
@@ -31,20 +43,6 @@ test('Post detail page has correct title when post is not found', async ({
 
   await page.goto(`/post/${postSlug}`)
   await expect(page).toHaveTitle(POST_NOT_FOUND_MESSAGE)
-})
-
-test('Category page has correct title', async ({ page }) => {
-  const category = categories[0]
-  const { name } = category
-
-  await page.goto(`/categoria/${category.slug}`)
-  await expect(page).toHaveTitle(`${name} - ${TITLE}`)
-})
-
-test('Newsletter success page has correct title', async ({ page }) => {
-  // ? pass email query param to prevent redirect
-  await page.goto(`/newsletter-success?email=${TEST_EMAIL}`)
-  await expect(page).toHaveTitle(`Iscrizione completata - ${TITLE}`)
 })
 
 test('Unsubscribe page has correct title', async ({ page }) => {
