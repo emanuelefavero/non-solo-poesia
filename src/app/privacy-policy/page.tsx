5bb4d43ab189692b4404a8edc372c89a55f4a2ed
Emanuele Favero <emafaux89@gmail.com>
2025-03-14 04:44:54 +0100

    add title and description metadata to privacy-policy page

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,3 +1,14 @@
+import { TITLE } from '@/data/title'
+import type { Metadata } from 'next'
+
+// * Metadata
+export const metadata: Metadata = {
+  title: `Informativa sulla Privacy - ${TITLE}`,
+  description: `La nostra informativa sulla privacy ti spiega come raccogliamo, utilizziamo e proteggiamo i tuoi dati personali`,
+}
+
+// TODO Add unit and e2e tests for this page and privacy policy link
+
 export default function Page() {
   return (
     <>
