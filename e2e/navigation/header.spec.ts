6471f00bdbeea0988e9ea74064ae54edea41f67b
Emanuele Favero <emafaux89@gmail.com>
2025-03-14 01:43:48 +0100

    add e2e test - Clicking on a category link navigates to the category page

--- e2e/navigation/header.spec.ts
+++ e2e/navigation/header.spec.ts
@@ -1,3 +1,4 @@
+import { categories } from '@/data/categories'
 import { expect, test } from '@playwright/test'
 
 test('Clicking on the search icon navigates to the search page', async ({
@@ -12,4 +13,12 @@ test('Clicking on the logo navigates to the homepage', async ({ page }) => {
   await page.goto('/cerca')
   await page.click('header a[href="/"]')
   await expect(page).toHaveURL('/')
+})
+
+test('Clicking on a category link navigates to the category page', async ({
+  page,
+}) => {
+  await page.goto('/')
+  await page.click(`[aria-label="Vai a ${categories[0].name}"]`)
+  await expect(page).toHaveURL(`/categoria/${categories[0].slug}`)
 })
