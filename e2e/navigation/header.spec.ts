f8b1942d2954b0967f581ca99ea099a751210156
Emanuele Favero <emafaux89@gmail.com>
2025-03-17 23:16:11 +0100

    add e2e test - Clicking on the developer link navigates to the author page

--- e2e/navigation/header.spec.ts
+++ e2e/navigation/header.spec.ts
@@ -30,3 +30,18 @@ test('Clicking on privacy policy link navigates to the privacy policy page', asy
   await page.click('footer a[href="/informativa-sulla-privacy"]')
   await expect(page).toHaveURL('/informativa-sulla-privacy')
 })
+
+test('Clicking on the developer link navigates to the author page', async ({
+  page,
+  context,
+}) => {
+  // NOTE: The link opens in a new tab so we need to listen for the new page event
+  await page.goto('/')
+  const [newPage] = await Promise.all([
+    context.waitForEvent('page'), // ? listen for the new page event
+    page.click('footer a[href="https://emanuelefavero.com/"]'),
+  ])
+
+  await newPage.waitForLoadState() // ? wait for the new page to load
+  await expect(newPage).toHaveURL('https://emanuelefavero.com/')
+})
