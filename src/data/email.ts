001a05fe36919ed809ab8f1d202516d72a30ba91
Emanuele Favero <emafaux89@gmail.com>
2025-03-19 14:40:59 +0100

    change test email and amdin id

--- src/data/email.ts
+++ src/data/email.ts
@@ -1,4 +1,4 @@
 const email = 'info@nonsolopoesia.it'
 export const CONTACT_EMAIL = email
 
-export const TEST_EMAIL = 'test@example.com' // ? fake email for testing
+export const TEST_EMAIL = 'jack+clerk_test@example.com' // ? fake email for testing
