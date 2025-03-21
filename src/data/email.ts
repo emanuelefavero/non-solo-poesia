a29be255415b6aa350e73c082e7c34c01be2fd17
Emanuele Favero <emafaux89@gmail.com>
2025-03-21 06:57:48 +0100

    change and move TEST_EMAIL to env variable for security

--- src/data/email.ts
+++ src/data/email.ts
@@ -1,4 +1,4 @@
 const email = 'info@nonsolopoesia.it'
 export const CONTACT_EMAIL = email
 
-export const TEST_EMAIL = 'jack+clerk_test@example.com' // ? fake email for testing
+export const TEST_EMAIL = process.env.TEST_EMAIL || ''
