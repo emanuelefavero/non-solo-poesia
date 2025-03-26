8a453cb3afe18bba240da4530ef1ce9d667581de
Emanuele Favero <emafaux89@gmail.com>
2025-03-26 04:37:42 +0100

    add e2e test - Modify post link is visible when authenticated

--- e2e/auth/auth-links.spec.ts
+++ e2e/auth/auth-links.spec.ts
@@ -5,3 +5,9 @@ test('Create new post link is visible when authenticated', async ({ page }) => {
   const link = page.locator('a[title="Crea nuovo post"]')
   await expect(link).toBeVisible()
 })
+
+test('Modify post link is visible when authenticated', async ({ page }) => {
+  await page.goto('/post/sogni-nella-notte')
+  const link = page.locator('a:has-text("Modifica")')
+  await expect(link).toBeVisible()
+})
