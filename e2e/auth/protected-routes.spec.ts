7490b2aecf70cffabb048af5de3d967d871553ed
Emanuele Favero <emafaux89@gmail.com>
2025-03-26 02:55:45 +0100

    add e2e test - Modify post page can be accessed when authenticated

--- e2e/auth/protected-routes.spec.ts
+++ e2e/auth/protected-routes.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from '@playwright/test'
 
+// * Authenticated tests
 test('Create new post page can be accessed when authenticated', async ({
   page,
 }) => {
@@ -11,4 +12,17 @@ test('Create new post page can be accessed when authenticated', async ({
 
   const h1 = page.locator('h1')
   await expect(h1).toContainText(/Crea nuovo post/i)
+})
+
+test('Modify post page can be accessed when authenticated', async ({
+  page,
+}) => {
+  // Navigate to the protected page
+  await page.goto('/modifica-post/sogni-nella-notte')
+
+  // Wait for the page to load
+  await page.waitForLoadState('domcontentloaded')
+
+  const h1 = page.locator('h1')
+  await expect(h1).toContainText(/Sogni nella Notte/i)
 })
