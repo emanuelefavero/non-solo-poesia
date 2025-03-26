b4e7a5bc4a4b6cc2d96197cb4137585230d79b04
Emanuele Favero <emafaux89@gmail.com>
2025-03-26 01:46:17 +0100

    add e2e test - Sign out with Clerk

--- e2e/auth/auth.spec.ts
+++ e2e/auth/auth.spec.ts
@@ -20,3 +20,35 @@ test('Sign in with Clerk', async ({ page }) => {
   const header = page.locator('header')
   await expect(header).toContainText(TEST_EMAIL)
 })
+
+test('Sign out with Clerk', async ({ page, browserName }) => {
+  test.skip(
+    browserName !== 'chromium',
+    'This test runs only in Chrome to prevent NS_BINDING_ABORTED error',
+  )
+
+  await page.goto('/')
+
+  // Sign in with email
+  await clerk.signIn({
+    page,
+    signInParams: {
+      strategy: 'email_code',
+      identifier: TEST_EMAIL,
+    },
+  })
+
+  // Wait for the sign-in to complete
+  await clerk.loaded({ page })
+
+  // Sign out
+  await clerk.signOut({ page })
+
+  // Go to a protected page and wait for the page to load
+  await page.goto('/crea-nuovo-post')
+  await page.waitForLoadState('domcontentloaded')
+
+  // Should be redirected to the home page
+  const h1 = page.locator('h1')
+  await expect(h1).toContainText(/Ultimi Post/i)
+})
