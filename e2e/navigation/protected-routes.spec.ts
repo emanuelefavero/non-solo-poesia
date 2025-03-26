b5dba2d5721bfbac8a0dc45981808d65e4b8d34b
Emanuele Favero <emafaux89@gmail.com>
2025-03-26 03:01:00 +0100

    add e2e test - Modify post page cannot be accessed when not authenticated

--- e2e/navigation/protected-routes.spec.ts
+++ e2e/navigation/protected-routes.spec.ts
@@ -9,3 +9,13 @@ test('Create new post page cannot be accessed when not authenticated', async ({
   const h1 = page.locator('h1')
   await expect(h1).toContainText(/Ultimi Post/i)
 })
+
+test('Modify post page cannot be accessed when not authenticated', async ({
+  page,
+}) => {
+  // Navigate to the protected page
+  await page.goto('/modifica-post/sogni-nella-notte')
+
+  const h1 = page.locator('h1')
+  await expect(h1).toContainText(/Ultimi Post/i)
+})
