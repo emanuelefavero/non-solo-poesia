b998cce7a329ffdcb02aa1d8be31d0b692a230bc
Emanuele Favero <emafaux89@gmail.com>
2026-01-01 08:04:26 +0100

    test(e2e): remove sign-in test with Clerk
    - To prevent future failures related to Clerk updating their output,
      the sign-in test has been removed
    - The sign-out test remains intact and continues to function correctly

--- e2e/auth/auth.spec.ts
+++ e2e/auth/auth.spec.ts
@@ -1,16 +1,5 @@
-import { TEST_EMAIL } from '@/data/email'
 import { clerk } from '@clerk/testing/playwright'
 import { expect, test } from '@playwright/test'
-
-test('Sign in with Clerk', async ({ page }) => {
-  // Navigate to an unprotected page that loads Clerk
-  await page.goto('/')
-
-  // Check if the header contains the email of the signed-in user
-  // NOTE: The email will be displayed only for users that are signed in with email (like our test user), other sign-in methods will display the user's name
-  const header = page.locator('header')
-  await expect(header).toContainText(TEST_EMAIL)
-})
 
 test('Sign out with Clerk', async ({ page }) => {
   await page.goto('/')
